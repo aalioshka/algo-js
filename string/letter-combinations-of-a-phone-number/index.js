380460e3db59f783b5ab5511359b1d848fe17f35
Aliaksei Alioshka <AAlioshka@slb.com>
2021-08-20 15:49:21 -0500

    https://leetcode.com/problems/letter-combinations-of-a-phone-number/

---
+++ string/letter-combinations-of-a-phone-number/index.js
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+
+const mappings = {
+    '2': ['a', 'b', 'c'],
+    '3': ['d', 'e', 'f'],
+    '4': ['g', 'h', 'i'],
+    '5': ['j', 'k', 'l'],
+    '6': ['m', 'n', 'o'],
+    '7': ['p', 'q', 'r', 's'],
+    '8': ['t', 'u', 'v'],
+    '9': ['w', 'x', 'y', 'z']
+};
+
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+
+    if (!digits || digits.length === 0) return [];
+    if (digits.length === 1) {
+        return mappings[digits];
+    }
+
+    let result = [];
+    let set1 = letterCombinations(digits.substr(0, 1));
+    let set2 = letterCombinations(digits.substr(1));
+
+    for (let i = 0; i < set1.length; i++) {
+        for (let j = 0; j < set2.length; j++) {
+            result.push(set1[i] + set2[j]);
+        }
+    }
+
+    return result;
+};
