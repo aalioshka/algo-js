581b3afdf80082b2d34f1b6d7e858953d0714528
Aliaksei Alioshka <AAlioshka@slb.com>
2021-09-10 13:53:19 -0500

    LRU cache

--- linked-list/doubly-linked-list/solutions/lru-cache/index2.js
+++ linked-list/doubly-linked-list/solutions/lru-cache/index2.js
@@ -1,49 +1,4 @@
 // inspired by https://leetcode.com/problems/lru-cache/discuss/617415/JavaScript-2-Solutions-(ES6-Map-vs-Doubly-linked-list)
-
-// Solution 1 - Using ES6 Map
-
-// /**
-//  * @param {number} capacity
-//  */
-// var LRUCache = function(capacity) {
-//     this.map = new Map();
-//     this.capacity = capacity;
-// };
-//
-// /**
-//  * @param {number} key
-//  * @return {number}
-//  */
-// LRUCache.prototype.get = function(key) {
-//     if (!this.map.has(key)) return -1;
-//     const val = this.map.get(key);
-//     this.map.delete(key);
-//     this.map.set(key, val);
-//     return val;
-// };
-//
-// /**
-//  * @param {number} key
-//  * @param {number} value
-//  * @return {void}
-//  */
-// LRUCache.prototype.put = function(key, value) {
-//     this.map.delete(key);
-//     this.map.set(key, value);
-//     if (this.map.size > this.capacity) {
-//         const firstItem = this.map.keys().next().value;
-//         this.map.delete(firstItem);
-//     }
-// };
-//
-// /**
-//  * Your LRUCache object will be instantiated and called as such:
-//  * var obj = new LRUCache(capacity)
-//  * var param_1 = obj.get(key)
-//  * obj.put(key,value)
-//  */
-
-// Solution 2 - Using Object and Doubly linked list
 // Solution 2 - Using Object and Doubly linked list
 function Node(key, val) {
     this.key = key;
