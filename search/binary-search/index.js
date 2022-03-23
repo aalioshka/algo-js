6fb5ce68c0fd40dbeebfd36c586eab0c0fccf7ab
Aliaksei Alioshka <aalishka@fieldware.com>
2022-03-23 11:59:44 -0500

    binary search review

--- search/binary-search/index.js
+++ search/binary-search/index.js
@@ -16,3 +16,30 @@ function binarySearch(arr, elem) {
 
     return arr[middle] === elem ? middle : -1;
 }
+
+const arr = [2,5,6,9,13,15,28,30];
+const states = [
+    'Alabama','Alaska','American Samoa','Arizona','Arkansas',
+    'California','Colorado','Connecticut','Delaware','District of Columbia',
+    'Federated States of Micronesia','Florida','Georgia','Guam',
+    'Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas',
+    'Kentucky','Louisiana','Maine','Marshall Islands','Maryland',
+    'Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana',
+    'Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York',
+    'North Carolina','North Dakota','Northern Mariana Islands','Ohio','Oklahoma',
+    'Oregon','Palau','Pennsylvania','Puerto Rico','Rhode Island','South Carolina',
+    'South Dakota','Tennessee','Texas','Utah','Vermont','Virgin Island','Virginia',
+    'Washington','West Virginia','Wisconsin','Wyoming'
+];
+
+let result = binarySearch(arr, 9);
+console.log(result); // 3
+
+result = binarySearch(arr, 25);
+console.log(result); // -1
+
+result = binarySearch(states, 'Virgin Island');
+console.log(result); // 53
+
+result = binarySearch(states, 'Barbados');
+console.log(result); // -1
