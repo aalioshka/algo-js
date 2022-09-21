9a2544f0ba9260f06fb4780d3bbcab3d193f81a7
Aliaksei Alioshka <aalishka@fieldware.com>
2022-09-21 14:59:12 -0700

    find-first-and-last-position-of-element-in-sorted-array added one more solution

--- 2022-revisit/binary-search/find-first-and-last-position-of-element-in-sorted-array.js
+++ 2022-revisit/binary-search/find-first-and-last-position-of-element-in-sorted-array.js
@@ -1,3 +1,7 @@
+// Solution 1: Binary search + 2 pointers
+// Complexity: O(NlogN), where N is a length of nums array.
+//     Not optimal since we can have something like 888888888
+
 /**
  * @param {number[]} nums
  * @param {number} target
@@ -30,4 +34,51 @@ var searchRange = function(nums, target) {
     while(nums[endIndex + 1] === target) endIndex++;
 
     return [startIndex, endIndex];
+};
+
+// Solution 2: 2 Binary searches
+// Complexity: O(logN), where N is a length of nums array.
+//     Optimal for something like 888888888
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange2 = function(nums, target) {
+    let startIndex = 0;
+    let endIndex = nums.length - 1;
+    let middleIndex;
+
+    // 1) find the startIndex - binary search
+    while(startIndex <= endIndex) {
+        middleIndex = Math.floor((startIndex + endIndex) / 2);
+        if (nums[middleIndex] >= target) {
+            endIndex = middleIndex - 1;
+        } else {
+            startIndex = middleIndex + 1;
+        }
+    }
+
+    if (nums[startIndex] !== target) {
+        return [-1, -1];
+    }
+
+    const start = startIndex;
+
+    // reset end
+    endIndex = nums.length - 1;
+
+    // 2) find the endIndex - binary search
+    while(startIndex <= endIndex) {
+        middleIndex = Math.floor((startIndex + endIndex) / 2);
+        if (nums[middleIndex] <= target) {
+            startIndex = middleIndex + 1;
+        } else {
+            endIndex = middleIndex - 1;
+        }
+    }
+
+
+    return [start, endIndex];
 };
