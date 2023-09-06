2580353906121b3cbf82eab49e0810afbfccd730
Aliaksei Alioshka <aalishka@fieldware.com>
2023-09-06 14:46:54 -0700

    reverse list

--- linked-list/solutions/reverse-linked-list/index.js
+++ linked-list/solutions/reverse-linked-list/index.js
@@ -10,45 +10,15 @@
  * @return {ListNode}
  */
 var reverseList = function(head) {
-    let previous = null;
-    let current = head;
-    let next = head;
-    
-    while(current) {
-        // 1) save reff from current to next
-        next = current.next;
-        
-        // 2) change pointer from right to the left
-        current.next = previous;
-        
-        // 3) now move both prev and curr to the right
-        previous = current;
-        current = next;
-        
-        // and repeat while loop until we rich end of the list
-    }
-    
-    return previous;
-};
+    let prev = null;
+    let curr = head;
 
-/*
-    start: ****
-             1 -> 2 -> 3 -> 4
-         p   c
-            (n)  n
-             p   c
-    null <- 1    2 -> 3 -> 4
-             p   c
-                (n)   n
-                 p    n
-            1 <- 2    3 -> 4
-                 p     n
-                      (n)  n
-                       p   c
-            1 <- 2 <-  3   4
-                       p   c
-                          (n)   n
-                           p    c
-            1 <- 2 <- 3 <- 4
-    end: ****
-*/
+    while (curr) {
+        let temp = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = temp;
+    }
+
+    return prev;
+};
