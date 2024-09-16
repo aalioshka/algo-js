0aef63ce3f8454612ae3809066b4c1d70b1f7658
Aliaksei Alioshka <aaleshka84@gmail.com>
2024-09-15 22:24:40 -0700

    lfc double linked list

--- linked-list/doubly-linked-list/solutions/lru-cache/index2.js
+++ linked-list/doubly-linked-list/solutions/lru-cache/index2.js
@@ -1,90 +1,61 @@
-// inspired by https://leetcode.com/problems/lru-cache/discuss/617415/JavaScript-2-Solutions-(ES6-Map-vs-Doubly-linked-list)
-// Solution 2 - Using Object and Doubly linked list
-function Node(key, val) {
-    this.key = key;
-    this.val = val;
-    this.next = null;
-    this.prev = null;
-}
+// https://neetcode.io/problems/lru-cache
 
-function DoublyLinkedList() {
-    this.head = null;
-    this.tail = null;
-    this.length = 0;
-
-    this.push = function(key, val) {
-        const newNode = new Node(key, val);
-        if(!this.head) {
-            this.head = newNode;
-            this.tail = newNode;
-        } else {
-            this.tail.next = newNode;
-            newNode.prev = this.tail;
-            this.tail = newNode;
-        }
-        this.length++;
-        return newNode;
-    }
-
-    this.remove = function(node) {
-        if(!node.next && !node.prev) { // if there's only 1 node
-            this.head === null;
-            this.tail === null;
-        } else if(!node.next) { // if the node is tail node
-            this.tail = node.prev;
-            this.tail.next = null;
-        } else if(!node.prev) { // if the node is head node
-            this.head = node.next;
-            this.head.prev = null;
-        } else { // if the node is in between
-            const beforeNode = node.prev;
-            const afterNode = node.next;
-            beforeNode.next = afterNode;
-            afterNode.prev = beforeNode;
-        }
-        this.length--;
+class Node {
+    constructor(key, val) {
+        this.key = key;
+        this.val = val;
+        this.prev = null;
+        this.next = null;
     }
 }
 
-/**
- * @param {number} capacity
- */
-var LRUCache = function(capacity) {
-    this.DLL = new DoublyLinkedList();
-    this.cache = {};
-    this.capacity = capacity;
-};
-
-/**
- * @param {number} key
- * @return {number}
- */
-LRUCache.prototype.get = function(key) {
-    if(!this.cache[key]) return -1;
-    const { val } = this.cache[key];
-    this.DLL.remove(this.cache[key]);
-    this.cache[key] = this.DLL.push(key, val);
-    return val;
-};
-
-/**
- * @param {number} key
- * @param {number} value
- * @return {void}
- */
-LRUCache.prototype.put = function(key, value) {
-    if(this.cache[key]) this.DLL.remove(this.cache[key]);
-    this.cache[key] = this.DLL.push(key, value);
-    if(this.DLL.length > this.capacity) {
-        const currKey = this.DLL.head.key;
-        delete this.cache[currKey];
-        this.DLL.remove(this.DLL.head);
+class LRUCache {
+    constructor(capacity) {
+        this.cap = capacity;
+        this.cache = new Map();
+        this.left = new Node(0, 0);
+        this.right = new Node(0, 0);
+        this.left.next = this.right;
+        this.right.prev = this.left;
     }
-};
 
-/**
- * Your LRUCache object will be instantiated and called as such:
- * var obj = new LRUCache(capacity)
- * var param_1 = obj.get(key)
- * obj.put(key,value)
- */
+    remove(node) {
+        const prev = node.prev;
+        const nxt = node.next;
+        prev.next = nxt;
+        nxt.prev = prev;
+    }
+
+    insert(node) {
+        const prev = this.right.prev;
+        prev.next = node;
+        node.prev = prev;
+        node.next = this.right;
+        this.right.prev = node;
+    }
+
+    get(key) {
+        if (this.cache.has(key)) {
+            const node = this.cache.get(key);
+            this.remove(node);
+            this.insert(node);
+            return node.val;
+        }
+        return -1;
+    }
+
+    put(key, value) {
+        if (this.cache.has(key)) {
+            this.remove(this.cache.get(key));
+        }
+        const newNode = new Node(key, value);
+        this.cache.set(key, newNode);
+        this.insert(newNode);
+
+        if (this.cache.size > this.cap) {
+            const lru = this.left.next;
+            this.remove(lru);
+            this.cache.delete(lru.key);
+        }
+    }
+}
