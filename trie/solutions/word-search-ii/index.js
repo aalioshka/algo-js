e6e86814e05c867d980288a665511610157c58e1
Aliaksei Alioshka <aaleshka84@gmail.com>
2024-09-20 18:26:58 -0700

    added trie examples

--- trie/solutions/word-search-ii/index.js
+++ trie/solutions/word-search-ii/index.js
@@ -1,7 +1,5 @@
-// https://leetcode.com/problems/word-search-ii
+// neetcode: https://neetcode.io/problems/search-for-word-ii
 
-// https://www.youtube.com/watch?v=7XmS8McW_1U
-// see detailed explanation about trie here
 /**
  * @param {character[][]} board
  * @param {string[]} words
@@ -9,46 +7,65 @@
  */
 var findWords = function(board, words) {
     let res = [];
-
+  
     const root = buildTrie(words);
     for (let row = 0; row < board.length; row++) {
-        for (let col = 0; col < board[0].length; col++) {
-            dfs(root, row, col, board, res);
-        }
+      for (let col = 0; col < board[0].length; col++) {
+        dfs(row, col, root);
+      }
     }
     return res;
-
-};
-
-function dfs(node, row, col, board, res) {
-    if (node.end) {
+      
+    function dfs(row, col, node) {
+      if (node.end) {
         res.push(node.end);
         node.end = null;   // make sure only print one time for each word
+      }
+  
+      if (row < 0 || row >= board.length || col < 0 || col >= board[0].length || !node[board[row][col]]) {
+          return;
+      }
+  
+      const c = board[row][col];
+      board[row][col] = '#';  // mark visited
+      dfs(row + 1, col, node[c]); // up
+      dfs(row - 1, col, node[c]); // down
+      dfs(row, col + 1, node[c]); // right
+      dfs(row, col - 1, node[c]); // left
+      board[row][col] = c;  // reset - back track
     }
-
-    if (row < 0 || row >= board.length || col < 0 || col >= board[0].length) return;
-    if (!node[board[row][col]]) return;
-
-    const c = board[row][col];
-    board[row][col] = '#';  // mark visited
-    dfs(node[c], row + 1, col, board, res); // up
-    dfs(node[c], row - 1, col, board, res); // down
-    dfs(node[c], row, col + 1, board, res); // right
-    dfs(node[c], row, col - 1, board, res); // left
-    board[row][col] = c;  // reset - back track
-}
-
+  
+};
+  
 function buildTrie(words) {
-    const root = {};
-    for (let w of words) {
+      const root = {};
+      for (let w of words) {
         let pointer = root; // here 'pointer' just a reference, that we use to go down from root till last child node
                             // and when we rich last child node - this is the end of the world
                             // and instead of setting "node.end = true", we set "node.end = word"
         for (let c of w) {
-            if (!pointer[c]) pointer[c] = {}; // if we already have such node, lets ignore it creating and just move the pointer
-            pointer = pointer[c];
+          if (!pointer[c]) pointer[c] = {}; // if we already have such node, lets ignore it creating and just move the pointer
+          pointer = pointer[c];
         }
-        pointer.end = w;
-    }
-    return root;
+        pointer.end = w; 
+      }
+      return root;
 }
+
+const board = 
+[
+    ["o","a","a","n"],
+    ["e","t","a","e"],
+    ["i","h","k","r"],
+    ["i","f","l","v"]
+];
+const words = ["oath","pea","eat","rain"];
+console.log(findWords(board, words)); // ["eat","oath"]
+
+const board2 = 
+[
+    ["a","b"],
+    ["c","d"]
+];
+const words2 = ["abcb"];
+console.log(findWords(board2, words2)); // []
