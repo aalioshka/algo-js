8a705bea75a619d7a6196813fe4f90e56da34d0b
Aliaksei Alioshka <aaleshka84@gmail.com>
2024-12-04 14:40:28 -0800

    rotten oranges

--- matrix/solutions/rotting-oranges/index.js
+++ matrix/solutions/rotting-oranges/index.js
@@ -3,8 +3,8 @@
 // In a given grid, each cell can have one of three values:
 //
 //     the value 0 representing an empty cell;
-// the value 1 representing a fresh orange;
-// the value 2 representing a rotten orange.
+//     the value 1 representing a fresh orange;
+//     the value 2 representing a rotten orange.
 //     Every minute, any fresh orange that is adjacent (4-directionally) to a rotten orange becomes rotten.
 //
 //     Return the minimum number of minutes that must elapse until no cell has a fresh orange.  If this is impossible, return -1 instead.
@@ -39,10 +39,9 @@
  */
 var orangesRotting = function(grid) {
     let depth = 0; // our minutes
-    let depthQueue = [];
     let queue = []; // to add rotten oranges
     let freshOranges = 0;
-
+    
     for(let row = 0; row < grid.length; row++){
         for(let col = 0; col < grid[row].length; col++){
             if(grid[row][col] === 1){
@@ -53,55 +52,57 @@ var orangesRotting = function(grid) {
             }
         }
     }
-
+    
     while (queue.length && freshOranges) {
-        depthQueue = []; // we start new depth
-
-        while(queue.length){
+        let size = queue.length; // we start new depth
+        
+        for(let i = 0; i < size; i++){
+            // The shift() method of Array instances removes the first element from an array and returns that removed element. 
+            // This method changes the length of the array.
             let [_row, _col] = queue.shift(); // rotten orange
             rotNeighbors(_row, _col);
         }
-
+        
         // we are done with current level/depth
         // lets up it and start traversing all neighbors we collected
         depth++;
-        queue = depthQueue;
     }
-
+    
     if(freshOranges !== 0){
         return -1
     }
     return depth;
-
+    
     // rot surrounding oranges
-    // Add them to the depthQueue
+    // Add them to the queue
     function rotNeighbors(row, col){
         // check up
-        if(grid[row][col+1] === 1){
-            freshOranges--;
-            grid[row][col+1] = 2;
-            depthQueue.push([row, col+1]);
-        }
-
-        // check down
-        if(grid[row][col-1] === 1){
-            freshOranges--;
-            grid[row][col-1] = 2;
-            depthQueue.push([row, col-1]);
-        }
-
-        // check left
-        if(grid[row-1] && grid[row-1][col] === 1){
-            freshOranges--;
-            grid[row-1][col] = 2;
-            depthQueue.push([row-1, col]);
-        }
-
-        // check right
         if(grid[row+1] && grid[row+1][col] === 1){
             freshOranges--;
             grid[row+1][col] = 2;
-            depthQueue.push([row+1, col]);
+            queue.push([row+1, col]);
+        }
+        
+        // check down
+        if(grid[row-1] && grid[row-1][col] === 1){
+            freshOranges--;
+            grid[row-1][col] = 2;
+            queue.push([row-1, col]);
+        }
+        
+        
+        // check right
+        if(grid[row][col+1] === 1){
+            freshOranges--;
+            grid[row][col+1] = 2;
+            queue.push([row, col+1]);
+        }
+
+        // check left
+        if(grid[row][col-1] === 1){
+            freshOranges--;
+            grid[row][col-1] = 2;
+            queue.push([row, col-1]);
         }
     }
 };
