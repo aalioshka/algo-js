d838d100041d114aa4a47657741fa500944768f0
Aliaksei Alioshka <aaleshka84@gmail.com>
2024-12-04 20:12:42 -0800

    max area of ilands

--- matrix/solutions/max-area-of-island/index.js
+++ matrix/solutions/max-area-of-island/index.js
@@ -1,25 +1,4 @@
-// https://leetcode.com/problems/max-area-of-island/discuss/494446/javascript-simple-brute-force-solution
-// 695. Max Area of Island
-// Given a non-empty 2D array grid of 0's and 1's, an island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
-//
-// Find the maximum area of an island in the given 2D array. (If there is no island, the maximum area is 0.)
-//
-// Example 1:
-//
-// [[0,0,1,0,0,0,0,1,0,0,0,0,0],
-//     [0,0,0,0,0,0,0,1,1,1,0,0,0],
-//     [0,1,1,0,1,0,0,0,0,0,0,0,0],
-//     [0,1,0,0,1,1,0,0,1,0,1,0,0],
-//     [0,1,0,0,1,1,0,0,1,1,1,0,0],
-//     [0,0,0,0,0,0,0,0,0,0,1,0,0],
-//     [0,0,0,0,0,0,0,1,1,1,0,0,0],
-//     [0,0,0,0,0,0,0,1,1,0,0,0,0]]
-// Given the above grid, return 6. Note the answer is not 11, because the island must be connected 4-directionally.
-//     Example 2:
-//
-// [[0,0,0,0,0,0,0,0]]
-// Given the above grid, return 0.
-// Note: The length of each dimension in the given grid does not exceed 50.
+// neetcode: https://neetcode.io/problems/max-area-of-island
 
 /**
  * @param {number[][]} grid
@@ -67,3 +46,16 @@ function exploreIsland(grid, i, j){
 
     return area;
 }
+
+const grid = [
+    [0,0,1,0,0,0,0,1,0,0,0,0,0],
+    [0,0,0,0,0,0,0,1,1,1,0,0,0],
+    [0,1,1,0,1,0,0,0,0,0,0,0,0],
+    [0,1,0,0,1,1,0,0,1,0,1,0,0],
+    [0,1,0,0,1,1,0,0,1,1,1,0,0],
+    [0,0,0,0,0,0,0,0,0,0,1,0,0],
+    [0,0,0,0,0,0,0,1,1,1,0,0,0],
+    [0,0,0,0,0,0,0,1,1,0,0,0,0]
+];
+
+console.log(maxAreaOfIsland(grid)) // 6
