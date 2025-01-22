9b1caa5fcae02ce929e7bd416d6c2f10163e26bf
Aliaksei Alioshka <aaleshka84@gmail.com>
2025-01-22 13:20:44 -0800

    max-area-of-island typos

--- matrix/solutions/max-area-of-island/index.js
+++ matrix/solutions/max-area-of-island/index.js
@@ -6,42 +6,42 @@
  */
 var maxAreaOfIsland = function(grid) {
     let max = 0;
-    for(let i = 0; i < grid.length; i++){
-        for(let j = 0; j < grid[0].length; j++){
-            if(grid[i][j] === 1){
-                grid[i][j] = -1; // mark as visited
-                max = Math.max(exploreIsland(grid, i, j), max);
+    for(let row = 0; row < grid.length; row++){
+        for(let col = 0; col < grid[0].length; col++){
+            if(grid[row][col] === 1){
+                grid[row][col] = -1; // mark as visited
+                max = Math.max(exploreIsland(grid, row, col), max);
             }
         }
     }
     return max;
 };
 
-function exploreIsland(grid, i, j){
+function exploreIsland(grid, row, col){
     let area = 1;
 
     // check up
-    if(grid[i][j+1] === 1){
-        grid[i][j+1] = -1;
-        area = area + exploreIsland(grid, i, j+1);
+    if(grid[row+1] && grid[row+1][col] === 1){
+        grid[row+1][col] = -1;
+        area = area + exploreIsland(grid, row+1, col);
     }
 
     // check down
-    if(grid[i][j-1] === 1){
-        grid[i][j-1] = -1;
-        area = area + exploreIsland(grid, i, j-1);
-    }
-
-    // check left
-    if(grid[i-1] && grid[i-1][j] === 1){
-        grid[i-1][j] = -1;
-        area = area + exploreIsland(grid, i-1, j);
+    if(grid[row-1] && grid[row-1][col] === 1){
+        grid[row-1][col] = -1;
+        area = area + exploreIsland(grid, row-1, col);
     }
 
     // check right
-    if(grid[i+1] && grid[i+1][j] === 1){
-        grid[i+1][j] = -1;
-        area = area + exploreIsland(grid, i+1, j);
+    if(grid[row][col+1] === 1){
+        grid[row][col+1] = -1;
+        area = area + exploreIsland(grid, row, col+1);
+    }
+
+    // check left
+    if(grid[row][col-1] === 1){
+        grid[row][col-1] = -1;
+        area = area + exploreIsland(grid, row, col-1);
     }
 
     return area;
