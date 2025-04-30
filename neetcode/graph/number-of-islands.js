5bdc5ad898cb662b31fa30d4168c998724e85690
Aliaksei Alioshka <aaleshka84@gmail.com>
2025-04-29 22:11:12 -0700

    graph number-of-islands

---
+++ neetcode/graph/number-of-islands.js
@@ -0,0 +1,85 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function(grid) {
+    let num = 0;
+    for(let row = 0; row < grid.length; row++) {
+        for(let col = 0; col < grid[row].length; col++) {
+            if(grid[row][col] === '1') {
+                grid[row][col] = -1; // mark as visited
+                num++;
+                
+                // 1) solution Depth First Search
+                dfs(grid, row, col);
+                
+                // 2) solution Breadth First Search
+                // bfs(grid, row, col); 
+            }
+        }
+    }
+    return num;
+};
+
+// For both dfs and bfs (dfs is better for space, bfs is recursion and it accumulate recursion call stack)
+// Time Complexity: O(m × n)
+// Space: O(m × n)
+
+function dfs(grid, rid, cid) {
+    // check up
+    if(grid[rid+1] && grid[rid+1][cid] === '1'){
+        grid[rid+1][cid] = -1; // mark as visited
+        dfs(grid, rid+1, cid);
+    }
+    
+    // check down
+    if(grid[rid-1] && grid[rid-1][cid] === '1') {
+        grid[rid-1][cid] = -1; // mark as visited
+        dfs(grid, rid-1, cid);
+    }
+    
+    // check right
+    if(grid[rid][cid+1] === '1') {
+        grid[rid][cid+1] = -1; // mark as visited
+        dfs(grid, rid, cid+1);
+    }
+    
+    // check left
+    if(grid[rid][cid-1] === '1') {
+        grid[rid][cid-1] = -1; // mark as visited
+        dfs(grid, rid, cid-1);
+    }
+}
+
+function bfs(grid, rid, cid) {
+    let queue = [[rid, cid]];
+    
+    while(queue.length) {
+        let [r, c] = queue.shift();
+        
+        // check up
+        if(grid[r+1] && grid[r+1][c] === '1'){
+            grid[r+1][c] = -1; // mark as visited
+            queue.push([r+1, c]);
+        }
+
+        // check down
+        if(grid[r-1] && grid[r-1][c] === '1') {
+            grid[r-1][c] = -1; // mark as visited
+            queue.push([r-1, c]);
+        }
+
+        // check right
+        if(grid[r][c+1] === '1') {
+            grid[r][c+1] = -1; // mark as visited
+            queue.push([r, c+1]);
+        }
+
+        // check left
+        if(grid[r][c-1] === '1') {
+            grid[r][c-1] = -1; // mark as visited
+            queue.push([r, c-1]);
+        }       
+    }
+    
+}
